a48df7b2ffa458d4c9f812981939aeb034c37be4
Aziz Shukuri <mraziz2007@gmail.com>
2023-06-07 00:54:45 -0700

    setup initial app

--- SCHOOL/STUDENTS/STUDENT_MANAGEMENT_SYSTEM/django_project/django_project/settings.py
+++ SCHOOL/STUDENTS/STUDENT_MANAGEMENT_SYSTEM/django_project/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)jovoii%hcu^0ld@d594s1csi_w&ou*w%w%^0yy*&l1vucje#q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student_mgmt_system',
 ]
 
 MIDDLEWARE = [
---
+++ SCHOOL/STUDENTS/STUDENT_MANAGEMENT_SYSTEM/django_project/student_mgmt_system/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StudentMgmtSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'student_mgmt_system'
