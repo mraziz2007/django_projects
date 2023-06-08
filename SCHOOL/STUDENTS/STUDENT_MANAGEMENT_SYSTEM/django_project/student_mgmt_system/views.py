13d7625d79579b82bc96d663b3de22c2db5c9b70
Aziz Shukuri <mraziz2007@gmail.com>
2023-06-08 16:09:26 -0700

    initial css and model for student

--- SCHOOL/STUDENTS/STUDENT_MANAGEMENT_SYSTEM/django_project/student_mgmt_system/views.py
+++ SCHOOL/STUDENTS/STUDENT_MANAGEMENT_SYSTEM/django_project/student_mgmt_system/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'student_mgmt_system/index.html')
+    return render(request, 'student_mgmt_system/index.html', {
+        'students': Student.objects.all()
+    })
