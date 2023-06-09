c009e5a80f934fb60928a4c2164ba215e57eaad4
Aziz Shukuri <mraziz2007@gmail.com>
2023-06-08 17:41:39 -0700

    revised index.html

--- SCHOOL/STUDENTS/STUDENT_MANAGEMENT_SYSTEM/django_project/student_mgmt_system/views.py
+++ SCHOOL/STUDENTS/STUDENT_MANAGEMENT_SYSTEM/django_project/student_mgmt_system/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Student
+import random
 
 # Create your views here.
 
-def index(request):
+def index(request): 
     return render(request, 'student_mgmt_system/index.html', {
-        'students': Student.objects.all()
+        'students': Student.objects.all(),
+        'color': random.choice(["active", "primary", "secondary", "success", "danger", "warning", "info", "light", "dark"])
     })
