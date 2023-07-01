6008f8587bdbb06e583933310d285654fbc292c6
mraziz2007@gmail.com <mraziz2007@gmail.com>
2023-07-01 15:03:00 -0700

    added new project for business

---
+++ BUSINESS/django_projects/BUSINESS_EXPENSE/authentication/urls.py
@@ -0,0 +1,7 @@
+from .views import RegistrationView, LoginView
+from django.urls import path
+
+urlpatterns = [
+    path('register', RegistrationView.as_view(), name="register"),
+    path('login', LoginView.as_view(), name="login"),
+]
---
+++ BUSINESS/django_projects/BUSINESS_EXPENSE/business_expense_dashboard/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BusinessExpenseDashboardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'business_expense_dashboard'
